30593f934075ed006bfd9d838a3efb2007a73264
Bruno <bruno.vigano@gmail.com>
2021-12-30 15:30:24 -0300

    feat: ensure signupController return an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,7 +1,15 @@
+interface retorno {
+  statusCode: number;
+  body: string | Error;
+}
 export default class SignUpController {
-  handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-    };
+  static handle(httpRequest: any): retorno {
+    let ret: retorno;
+
+    if (!httpRequest.body?.name) {
+      ret = { body: new Error('Missing param: name'), statusCode: 400 };
+    }
+
+    return ret;
   }
 }
