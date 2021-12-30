2a74ba77f6ba63432ad254f2eafa790fbd232a79
Bruno <bruno.vigano@gmail.com>
2021-12-30 17:43:44 -0300

    chore: eslint disable prefer default export

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,15 +3,16 @@ import { HttpRequest, HttpResponse } from '../protocols/http';
 export default class SignUpController {
   static handle(httpRequest: HttpRequest): HttpResponse {
     let ret: HttpResponse;
+    const { body } = httpRequest;
 
-    if (!httpRequest.body?.name) {
+    if (!body.name) {
       ret = {
         body: new Error('Missing param: name'),
         statusCode: 400,
       };
     }
 
-    if (!httpRequest.body?.email) {
+    if (!body.email) {
       ret = {
         body: new Error('Missing param: email'),
         statusCode: 400,
