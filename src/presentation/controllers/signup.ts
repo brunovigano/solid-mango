7980f8c75d32ac1f184e9a97f94620a251e9b7d0
Bruno <bruno.vigano@gmail.com>
2021-12-30 18:09:08 -0300

    feat: ensure SignUpController returns 400 if no password confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,7 +6,7 @@ export default class SignUpController {
   static handle(httpRequest: HttpRequest): HttpResponse {
     let ret: HttpResponse;
     const { body } = httpRequest;
-    const requiredParams = ['name', 'email', 'password'];
+    const requiredParams = ['name', 'email', 'password', 'passwordConfirmation'];
 
     for (const field of requiredParams) {
       if (!body[field]) {
