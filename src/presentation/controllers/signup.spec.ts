6ed156b85fbbaad1a8c9a9294c32d68beeb0ac08
Bruno <bruno.vigano@gmail.com>
2022-01-03 15:50:38 -0300

    test: add mockimplementation on emailvalidator returns 500 test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -18,16 +18,6 @@ const makeEmailValidator = () => {
   return new EmailValidatorStub();
 };
 
-const makeEmailValidatorWithError = () => {
-  class EmailValidatorStub implements EmailValidator {
-    isValid(email: string): boolean {
-      throw new Error();
-    }
-  }
-
-  return new EmailValidatorStub();
-};
-
 const makeSut = (): SutTypes => {
   const emailValidatorStub = makeEmailValidator();
   const sut = new SignUpController(emailValidatorStub);
@@ -135,8 +125,10 @@ describe('SignUp Controller', () => {
   });
 
   test('should return 500 if EmailValidator throws', () => {
-    const emailValidatorStub = makeEmailValidatorWithError();
-    const sut = new SignUpController(emailValidatorStub);
+    const { emailValidatorStub, sut } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error();
+    });
 
     const httpRequest = {
       body: {
