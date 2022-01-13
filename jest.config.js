96e067ea9c1c0a554b6b9b75772efc60cf2cbfd4
Bruno <bruno.vigano@gmail.com>
2022-01-13 18:46:58 -0300

    chore: update jest config to ignore protocols imports

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,15 @@ module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
   collectCoverage: false,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+  ],
   coverageDirectory: 'coverage',
   testEnviroment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
   },
-};
+}
