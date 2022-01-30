48c7880d1926fda76248dfc04346ddf912f3345c
Bruno Viganó <bruno.vigano@gmail.com>
2022-01-30 00:59:35 -0300

    test: ensure return 200 on login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
+import { hash } from 'bcrypt'
+import { Collection } from 'mongodb'
 import request from 'supertest'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
+
+let accountCollection: Collection
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,26 @@ describe('Login Routes', () => {
           email: 'bruno.vigano@gmail.com',
           password: '123',
           passwordConfirmation: '123',
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('should return 200 on login', async () => {
+      const password = await hash('123', 12)
+
+      await accountCollection.insertOne({
+        name: 'Bruno',
+        email: 'bruno.vigano@gmail.com',
+        password,
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'bruno.vigano@gmail.com',
+          password: '123',
         })
         .expect(200)
     })
