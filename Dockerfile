45944fb7eb5a286a18af1b7aa70346685f0ff83b
Bruno Viganó <bruno.vigano@gmail.com>
2022-02-07 18:47:11 -0300

    chore: change dev script to use docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:17
 WORKDIR /usr/src/solid-mango
 COPY ./package.json .
 RUN yarn --prod
-COPY ./dist ./dist
-EXPOSE 5000
-CMD yarn start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/solid-mango',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/solid-mango',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'bj#vWk6J9!fxBR',
   salt: Number(process.env.SALT) || 12,
