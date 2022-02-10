b9c9d50f2b553e2caf75de5cb736a86949715a55
Bruno <bruno.vigano@compasso.com.br>
2022-02-10 16:15:27 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -6,6 +6,30 @@ import {
 } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
 
+interface SutTypes {
+  validationStub: Validation
+  sut: AddSurveyController
+}
+
+const makeValidationStub = () => {
+  class ValidationStub implements Validation {
+    validate(input: any): Error {
+      return null
+    }
+  }
+  return new ValidationStub()
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidationStub()
+  const sut = new AddSurveyController(validationStub)
+
+  return {
+    sut,
+    validationStub,
+  }
+}
+
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     question: 'any_question',
@@ -15,14 +39,8 @@ const makeFakeRequest = (): HttpRequest => ({
 
 describe('AddSurvey Controller', () => {
   test('should call Validation with correct values ', async () => {
-    class ValidationStub implements Validation {
-      validate(input: any): Error {
-        return null
-      }
-    }
-    const validationStub = new ValidationStub()
+    const { sut, validationStub } = makeSut()
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const sut = new AddSurveyController(validationStub)
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
