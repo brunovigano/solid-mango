8122ff72091015663a3e0610b844959fa60911d0
Bruno Viganó <bruno.vigano@gmail.com>
2023-03-20 18:07:07 -0300

    test: ensure save result route return 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -1,35 +1,38 @@
 import { Collection } from 'mongodb'
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
 import app from '@/main/config/app'
+import { env } from '../config/env'
 
 let surveyResultCollection: Collection
+let surveyCollection: Collection
+let accountCollection: Collection
 
-// const makeAccessToken = async (): Promise<string> => {
-//   const account = await accountCollection.insertOne({
-//     name: 'Bruno',
-//     email: 'bruno.vigano@gmail.com',
-//     password: '123',
-//     role: 'admin',
-//   })
+const makeAccessToken = async (): Promise<string> => {
+  const account = await accountCollection.insertOne({
+    name: 'Bruno',
+    email: 'bruno.vigano189@gmail.com',
+    password: '123',
+  })
 
-//   const { jwtSecret } = env
-//   const id = account.ops[0]._id
-//   const accessToken = sign({ id }, jwtSecret)
+  const { jwtSecret } = env
+  const id = account.ops[0]._id
+  const accessToken = sign({ id }, jwtSecret)
 
-//   await accountCollection.updateOne(
-//     {
-//       _id: id,
-//     },
-//     {
-//       $set: {
-//         accessToken,
-//       },
-//     }
-//   )
+  await accountCollection.updateOne(
+    {
+      _id: id,
+    },
+    {
+      $set: {
+        accessToken,
+      },
+    }
+  )
 
-//   return accessToken
-// }
+  return accessToken
+}
 
 describe('Survey Routes', () => {
   beforeAll(async () => {
@@ -43,6 +46,10 @@ describe('Survey Routes', () => {
   beforeEach(async () => {
     surveyResultCollection = await MongoHelper.getCollection('surveyResult')
     await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
   })
 
   describe('PUT /surveys/:surveyId/results', () => {
@@ -55,27 +62,21 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
-    // test('should return 204 on add survey with valid token', async () => {
-    //   const accessToken = await makeAccessToken()
-    //   await request(app)
-    //     .post('/api/surveys')
-    //     .set('x-access-token', accessToken)
-    //     .send({
-    //       question: 'Question',
-    //       answers: [{ answer: 'Answer 1', image: 'http://image-name.com' }, { answer: 'Answer 2' }],
-    //     })
-    //     .expect(204)
-    // })
+    test('should return 200 on save survey result with token', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{ answer: 'Answer 1', image: 'http://image-name.com' }, { answer: 'Answer 2' }],
+        date: new Date(),
+      })
+
+      await request(app)
+        .put(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .send({
+          answer: 'Answer 1',
+        })
+        .expect(200)
+    })
   })
-
-  // describe('GET /surveys', () => {
-  //   test('should return 403 on load all survey without access token', async () => {
-  //     await request(app).get('/api/surveys').expect(403)
-  //   })
-
-  //   test('should return 204 on load surveys with valid access token', async () => {
-  //     const accessToken = await makeAccessToken()
-  //     await request(app).get('/api/surveys').set('x-access-token', accessToken).expect(204)
-  //   })
-  // })
 })
