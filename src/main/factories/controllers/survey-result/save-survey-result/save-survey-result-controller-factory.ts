790291dd2511aa50e5080a1f14c76321161f780f
Bruno Viganó <bruno.vigano@gmail.com>
2023-03-20 16:10:48 -0300

    feat: ensure SaveSurveyResultRout returns 403 if no token is provided

---
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -0,0 +1,13 @@
+import { Controller } from '@/presentation/protocols'
+import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { SaveSurveyResultController } from '@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller'
+import { makeDbLoadSurveysById } from '@/main/factories/usecases/survey/load-survey-by-id/db-load-survey-by-id-factory'
+import { makeDbSaveSurveyResult } from '@/main/factories/usecases/survey-result/save-survey-result/db-save-survey-result-factory'
+
+export const makeSaveSurveyResultController = (): Controller => {
+  const controller = new SaveSurveyResultController(
+    makeDbLoadSurveysById(),
+    makeDbSaveSurveyResult()
+  )
+  return makeLogControllerDecorator(controller)
+}
