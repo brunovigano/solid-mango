d926d37f03d5de3ba25f13912b93a14af7a1db43
Bruno Viganó <bruno.vigano@gmail.com>
2023-05-12 23:44:47 -0300

    docs: add swagger api

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,6 +6,6 @@ import { env } from './config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log(`Server running at ${env.port}`))
+    app.listen(env.port, () => console.log(`⚡ Server running at ${env.port}`))
   })
   .catch(console.error)
