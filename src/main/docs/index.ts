28e1cfff53fd31ff69e4c1527c308e672cbd5a16
Bruno Viganó <bruno.vigano@gmail.com>
2023-05-17 00:42:11 -0300

    docs: add load survey api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,14 @@
-import { badRequest, notFound, serverError, unauthorized } from './components'
-import { loginPath } from './paths'
-import { accountSchema, errorSchema, loginParamsSchema } from './schemas'
+import { badRequest, forbidden, notFound, serverError, unauthorized } from './components'
+import { loginPath, surveyPath } from './paths'
+import {
+  accountSchema,
+  apiKeyAuthSchema,
+  errorSchema,
+  loginParamsSchema,
+  surveyAnswerSchema,
+  surveySchema,
+  surveysSchema,
+} from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -22,19 +30,30 @@ export default {
     {
       name: 'Login',
     },
+    {
+      name: 'Enquete',
+    },
   ],
   paths: {
     '/login': loginPath,
+    '/surveys': surveyPath,
   },
   schemas: {
     account: accountSchema,
     loginParams: loginParamsSchema,
     error: errorSchema,
+    surveys: surveysSchema,
+    survey: surveySchema,
+    surveyAnswer: surveyAnswerSchema,
   },
   components: {
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema,
+    },
     badRequest,
     serverError,
     unauthorized,
     notFound,
+    forbidden,
   },
 }
